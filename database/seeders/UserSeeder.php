93a5aa67bc55f881b4b328290d8893b324aa4592
imyhacker <arifarhan1602@gmail.com>
2022-03-14 21:48:11 +0700

    Update tag berita dan user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -17,7 +18,7 @@ class UserSeeder extends Seeder
         \App\Models\User::create([
             'name' => 'Eci',
             'email' => 'eci@gmail.com',
-            'password' => bcrypt('1234578'),
+            'password' => Hash::make('12345678'),
             
         ]);
     }
